fbf317bc271e18d87a292d66c5d5efd5a28fb4da
mintus <sts.mnts@gmail.com>
2026-02-16 13:20:22 +0200

    Deploy from dist

--- assets/BufferResource-oHE9Sp05.js
+++ assets/BufferResource-oHE9Sp05.js
@@ -1,4 +1,4 @@
-import{w as U,m as g,n as c,u as S,B as _,o as m,E as I,p as h}from"./index-DjR_NArR.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,m as g,n as c,u as S,B as _,o as m,E as I,p as h}from"./index-Cxfm5EaO.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
